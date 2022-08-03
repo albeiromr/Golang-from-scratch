cd41aff5aa69c25b6a3cc37c6182528ce2fbedcb
Albeiro Marìn <xlandru@hotmail.com>
2022-08-02 19:16:41 -0500

    Declaración de constantes

--- main.go
+++ main.go
@@ -2,31 +2,23 @@ package main
 
 import "fmt"
 
-//variable global, se declara así por que
-//que sea una variable global o por qué queremos
-//definir claramente de que tipo debe ser,
-//es decir que golang no infiera el tipado
-var age int8 = 30
-//las variables con var no necesariamente deben ser
-//inicializadas
-var money int16
-//las variables con var podrían no llevar el tipo de dato
-var children = 0
+//así se declaran las constantes en golang
+//después de declaradas son imutables
+const iva int8 = 19
+
+//también se pueden declarar conjuntos de constantes como se realiza a continuación
+const (
+	diasRenta  int16   = 365
+	topeDinero float32 = 54
+)
 
 func main() {
 
-	//las variables con var también pueden declararse dentro 
-	//de funciones, esto por ejemplo para evitar inferir el 
-	//tipado
-	var name string = "Albeiro"
+	//las constantes también se pueden declarar dentro de funciones
+	const salarioMensualTope float32 = 4.200
 
-	//tambien se pueden declarar variables sin la palabra var
-	//estas deben declararse con la notación :=, la cual es 
-	//llamada oficialmente short assignment statement, estas
-	//estas funciones no pueden ser declaradas de forma global
-	//como se hace con las de var
-	id := 236563
+	fmt.Println("El impuestao del iva en colombia es del ", iva, "%", "deben declarar renta cada",
+		diasRenta, "y el tope de dinero es ", topeDinero, "millones")
 
-
-	fmt.Println("Hello", name, "your age is", age, "and your id is", id)
+	fmt.Println("Quienes ganen mas de", salarioMensualTope, " millones deben pagar renta")
 }
