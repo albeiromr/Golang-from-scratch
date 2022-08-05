7c539b88b215df485ff5531bdb15d775db974b84
Albeiro Marìn <xlandru@hotmail.com>
2022-08-04 19:16:53 -0500

    Escape Characters or scape sequences

--- main.go
+++ main.go
@@ -4,36 +4,30 @@ import (
 	"fmt"
 )
 
-
 func main() {
 
-	//la función fmt.Sprintf es el equivalente a los templates strings en javascript
-	//retorna el string ya formateado y se le puede asignar a una variable, no imprime
-	// nada en consola
+	//Escape Characters or scape sequences
 
-	iva := 19
-	grados := 32
-
-	fraseImpuesto := fmt.Sprintf("El iva en colombia es %v%%", iva)
-	fmt.Println(fraseImpuesto)
-
-	fraseGrados := fmt.Sprintf("la temperatura en mariquita es %v° centigrados", grados)
-	fmt.Println(fraseGrados)
+    //se usan para insertar caracteres que son ilegales en un string, son como las entidades en html
+	//estan presentes en muchos lenguajes de programación, a continuación los que encontré relevantes
 
 
-	//-----------------------------------------------------------------------------------------------------------------------------------------
-	
-	//la función fmt.Printf imprime los valores según el formato o la transformación que 
-	//le queramos dar, los valores se asignan de izquierda a derecha el primer format verb
-	//corresponde a la primera variable declarada a la derecha y así susesivamente
 
-	//cada tipo de dato tiene format verbs distintos que se pueden consultar aquí https://pkg.go.dev/fmt
-	//también se pueden juntar dos verbos o mas como en el caso de la variable porcentaje
+	// (\") Double quote -----------
+	fmt.Println("scape sequence para crear \"comillas dobles\" dentro de un string")
+	//output: esta es una "línea" con un scape character
 
-	porcentajeDecimal := 5632.5632
-	porcentaje := 56
-	texto := "Hola a todos"
-	booleano := false
+	// (\\) back-slash -----------
+	fmt.Println("scape sequence para crear un back-slash \\ dentro de un string")
+	//output: cape sequence para crear un back-slash \ dentro de un string
 
-	fmt.Printf("el decimal es %2.2f%% el texto es %s el booleano es %t el porcentaje es %d%% ", porcentajeDecimal, texto, booleano, porcentaje)
+	// (\b) retroceder un espacio -----------
+	fmt.Println("el porcentaje es del", 5, "\b%")
+	//output: el porcentaje es del 5%
+
+	// (\n) salto de línea -----------
+	fmt.Println("esta es la primera línea, \n esta es la segunda línea")
+	//esta es la primera línea,
+	//esta es la segunda línea
+
 }
