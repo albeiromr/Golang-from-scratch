883ccbf3e41e7b90704035b3f831f190f3611e5b
Albeiro Marìn <xlandru@hotmail.com>
2022-08-05 17:05:23 -0500

    Bucles For

--- main.go
+++ main.go
@@ -2,61 +2,22 @@ package main
 
 import (
 	"fmt"
-	"runtime"
 )
 
-const (
-	existError      bool   = true
-	counter         int8   = 4
-	operativeSystem string = runtime.GOOS
-	age             int8   = 50
-)
+//esto es un slice, aún no entiendo este tipo de dato
+var testSlice = []int{5, 6, 7, 8, 9}
+
 
 func main() {
 
-	//este es un if else normal
-	if existError == false {
-		fmt.Println("no hay error")
-	} else {
-		fmt.Println("si hay error")
-	}
-	//este es un switch con una variable para hacer comparación
-	switch counter {
-	case 0:
-		fmt.Println("the counter is cero")
-	case 1:
-		fmt.Println("the counter is uno")
-	case 2:
-		fmt.Println("the counter is dos")
-		//los cases pueden tener uno o mas valores para comparar como a continuación
-	case 3, 4, 5:
-		fmt.Println("the counter is more than 2")
-	default:
-		fmt.Println("no es ninguno de los valores esperados")
-
-	}
-	//este es un switch que no tiene una variable para hacer comparación
-	//la comparación se puede hacer en cada case individual lo cual puede ayudar a simplificar
-	//anidaciones feas de if else
-	switch {
-	case operativeSystem == "Linux":
-		fmt.Println("tu sistema operativo es linux")
-	case operativeSystem == "darwin":
-		fmt.Println("tu sistema operativo es darwin")
-	default:
-		fmt.Println("tu sistema operativo es otro")
-	}
-	//la palabra fallthrough permite que el código siga haciendo validaciones
-	//para ver en que otro case puede haber una coincidencia. por ejemplo
-	//en el siguiente switch se imprimen 2 valores
-	switch {
-	case age > 40:
-		fmt.Println("ya estas como viejo")
-		fallthrough
-	case age == 50:
-		fmt.Println("tienes 50 años")
-	default:
-		fmt.Println("no se que edad tienes")
+	//a continuación un bucle for comun en go
+	for i := 0; i < 10; i++ {
+		fmt.Println(i + 1)
 	}
 
+	//este for es muy parecido al for each de javascript, no necesita saber el lenght de el array
+	//y nos devuelve también el index
+	for index, value := range testSlice {
+		fmt.Printf("el index es %v y el valor es %v \n", index, value)
+	}
 }
