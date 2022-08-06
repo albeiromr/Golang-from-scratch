d068a67419e84eda49730a5217820bc438a9adea
Albeiro Marìn <xlandru@hotmail.com>
2022-08-06 11:35:21 -0500

    Concurrencia, ejemplo sencillo

--- main.go
+++ main.go
@@ -2,25 +2,28 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
+//paralelismo es hacer varias cosas al mismo tiempo
+//concurrencia es ocuparse de varias cosas al mismo tiempo
+
+func hello(number int) {
+	fmt.Println("Hello number", number)
+	//time.sleep es como un setTimeout en javascript
+	time.Sleep(5000 * time.Millisecond)
+}
 
 func main() {
 
-	//forma cencilla de obtener el input del usuario desde la misma terminal
-	var name string
-	var age int8
-	fmt.Printf("introduce your name:  ")
-	fmt.Scanln(&name)
-	fmt.Printf("introduce your age:  ")
-	fmt.Scanln(&age)
-
-	switch {
-	case age < 18:
-		fmt.Printf("%v no eres mayor de 18 años, lo sentimos \n", name)
-	case age > 18:
-		fmt.Printf("%v eres mayor de 18 años, puedes acceder al servicio \n", name)
-	default:
-		fmt.Println("no hemos podido ver tu edad")
+	//para crear un goroutine usamos la palabra reservada go
+	for i := 0; i < 100; i++ {
+		go hello(i)
 	}
+
+	//las siguientes dos líneas son de rrelleno
+	//para que el proceso de la función main no termine antes
+	//que la ejecución del goroutine
+	var s string
+	fmt.Scanln(&s)
 }
