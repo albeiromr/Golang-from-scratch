ab736e0d8b0f9fa91e07f69a83e2c4fa07494887
Albeiro Marìn <xlandru@hotmail.com>
2022-08-08 12:34:55 -0500

    Numeric data types

--- main.go
+++ main.go
@@ -1,49 +1,17 @@
-package main
-
-import (
-	"errors"
-	"fmt"
-	"strconv"
-)
-
-//devolviendo un error personalizado con  errors.new()
-func checkInt(a,b int) error {
-	if a == 0 && b == 0 {
-		return errors.New("Error personalizado: los valores no pueden estar en cero")
-	}
-	return nil
-}
-
-//devolviendo un error formateado con fmt.Errorf()
-func formatedError(a int) error {
-	if a == 0 {
-		return fmt.Errorf("Error formateado: no puedes inicializar con un paremetro en %v", a)
-	}
-	return nil
-}
 
 
-func main() {
+//int      si el sistema es de 32 bits es un int32 y si es de 64 bits es un int64
+//int8     de -128 a 127
+//int16    de -32.768 hasta 32.767
+//int32    de -2.147.483.648 hasta 2.147.483.647
+//int64    de -9.223.372.036.854.776.000 hata 9.223.372.036.854.770.999
 
-	personalizedError := checkInt(0,0)
-	if personalizedError != nil {
-		fmt.Println(personalizedError )
-	} else {
-		fmt.Println("no existen errores")
-	}
+//uint     si el sistema es de 32 bits es un int32 y si es de 64 bits es un int64
+//uint8    de 0 hasta 255
+//uint16   de 0 hasta 65.535
+//uint32   de 0 hasta 4.294.967.295
+//unit64   de 0 18.446.744.073.709.551.999
 
-	formatedError := formatedError(0)
-	if formatedError  != nil {
-		fmt.Println(formatedError)
-	} else {
-		fmt.Println("no existen errores")
-	}
+//float32  ocupa 32 bits en memoria
+//float64  ocupa 64 bits en memoria y es el valor por defecto para números flotantes
 
-	//convirtiendo un string a un número, si no se puese convertir arroja un error
-	integer, parseError := strconv.Atoi("1234")
-	if parseError != nil {
-		fmt.Println(parseError)
-	} else {
-		fmt.Println( "The integer is ", integer)
-	}
-}
